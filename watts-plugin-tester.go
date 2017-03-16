d312717f10b2daa16f27a87c272f156a84a76b88
Lukas Burgey <ubedv@student.kit.edu>
2017-03-16 10:04:11 +0100

    Escape the right json object

--- watts-plugin-tester.go
+++ watts-plugin-tester.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-	"os"
-	"os/exec"
-	"fmt"
-	"gopkg.in/alecthomas/kingpin.v2"
+	"bytes"
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	v "github.com/gima/govalid/v1"
-	"io/ioutil"
 	"github.com/imdario/mergo"
 	"github.com/kalaspuffar/base64url"
-	"bytes"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"io/ioutil"
+	"os"
+	"os/exec"
 )
 
 type UserInfo struct {
 	FamilyName string `json:"family_name"`
-	Gender string `json:"gender"`
-	GivenName string`json:"given_name"`
-	ISS string `json:"iss"`
-	Name string `json:"name"`
-	Sub string `json:"sub"`
+	Gender     string `json:"gender"`
+	GivenName  string `json:"given_name"`
+	ISS        string `json:"iss"`
+	Name       string `json:"name"`
+	Sub        string `json:"sub"`
 }
 
 type PluginInput struct {
-	WattsVersion string `json:"watts_version"`
-	Action string `json:"action"`
-	ConfParams string `json:"conf_params"`
-	Params string `json:"params"`
-	CredState string `json:"cred_state"`
+	WattsVersion    string   `json:"watts_version"`
+	Action          string   `json:"action"`
+	ConfParams      string   `json:"conf_params"`
+	Params          string   `json:"params"`
+	CredState       string   `json:"cred_state"`
 	UserInformation UserInfo `json:"user_info"`
-	WattsUserid string `json:"watts_userid"`
+	WattsUserid     string   `json:"watts_userid"`
 }
 
 type User struct {
@@ -38,51 +38,50 @@ type User struct {
 	Subject string `json:"subject"`
 }
 
-type Output struct{
+type Output struct {
 	M map[string]string `json:"meta"`
-	O json.RawMessage `json:"output"`
+	O json.RawMessage   `json:"output"`
 }
 
-type ErrorOutput struct{
-	Meta map[string]string `json:"meta"`
-	ErrorString string `json:"error"`
-	InvalidOutput string `json:"invalid_output"`
+type ErrorOutput struct {
+	Meta          map[string]string `json:"meta"`
+	ErrorString   string            `json:"error"`
+	InvalidOutput string            `json:"invalid_output"`
 }
 
 var (
 	version = "0.1"
 
-	app = kingpin.New("watts-plugin-tester", "Test tool for watts plugins")
-	pluginTestAction = app.Flag("plugin-action", "The plugin action to be tested. Defaults to \"parameter\"").Default("parameter").Short('a').String()
-	printVersion = app.Command("version", "Print the version information")
+	app                 = kingpin.New("watts-plugin-tester", "Test tool for watts plugins")
+	pluginTestAction    = app.Flag("plugin-action", "The plugin action to be tested. Defaults to \"parameter\"").Default("parameter").Short('a').String()
+	printVersion        = app.Command("version", "Print the version information")
 	pluginInputOverride = app.Flag("json", "Use user provided json to override the inbuilt one").Short('j').String()
 	//verbose = app.Flag("verbose", "Be verbose").Short('v').Bool()
 	machineReadable = app.Flag("machine", "Be machine readable (all output will be json)").Short('m').Bool()
 
-	pluginTest = app.Command("test", "Test a plugin")
+	pluginTest     = app.Command("test", "Test a plugin")
 	pluginTestName = pluginTest.Arg("pluginName", "Name of the plugin to test").Required().String()
 
-	printDefault = app.Command("default", "Print the default plugin input as json")
+	printDefault  = app.Command("default", "Print the default plugin input as json")
 	printSpecific = app.Command("specific", "Print the plugin input (including the user override) as json")
-
 
 	defaultUserInfo = UserInfo{
 		FamilyName: "Mustermann",
-		Gender: "Male",
-		GivenName: "Max",
-		ISS: "https://issuer.example.com",
-		Name: "Max Mustermann",
-		Sub: "123456789",
+		Gender:     "Male",
+		GivenName:  "Max",
+		ISS:        "https://issuer.example.com",
+		Name:       "Max Mustermann",
+		Sub:        "123456789",
 	}
 	defaultPluginInput = PluginInput{
-		WattsVersion: "1.0.0",
-		ConfParams: "{}",
-		Params: "{}",
-		CredState: "undefined",
-		UserInformation: defaultUserInfo ,
+		WattsVersion:    "1.0.0",
+		ConfParams:      "{}",
+		Params:          "{}",
+		CredState:       "undefined",
+		UserInformation: defaultUserInfo,
 	}
 
-	schemes =  map[string]v.Validator{
+	schemes = map[string]v.Validator{
 		"parameter": v.Object(
 			v.ObjKV("result", v.String(v.StrIs("ok"))),
 			v.ObjKV("conf_params", v.Array(v.ArrEach(
@@ -138,25 +137,24 @@ var (
 
 func (p *PluginInput) generateUserID() {
 	userIdJson := map[string]string{
-		"issuer": p.UserInformation.ISS,
+		"issuer":  p.UserInformation.ISS,
 		"subject": p.UserInformation.Sub,
 	}
 	j, _ := json.Marshal(userIdJson)
-	p.WattsUserid= base64url.Encode(j)
+	escaped := bytes.Replace(j, []byte{'/'}, []byte{'\\', '/'}, -1)
+	p.WattsUserid = base64url.Encode(escaped)
 	return
 }
 
 func marshalPluginInput(p PluginInput) (s []byte) {
 	s, _ = json.MarshalIndent(p, "", "    ")
-	s = bytes.Replace(s, []byte{'/'}, []byte{'\\','/'}, -1)
-
 	return
 }
 
 func specificJson(p PluginInput) (pi PluginInput) {
 	if *pluginInputOverride != "" {
 		inputOverride, err := ioutil.ReadFile(*pluginInputOverride)
-		if err !=  nil {
+		if err != nil {
 			// TODO machine readability
 			fmt.Println("Error reading file ", *pluginInputOverride, " (", err, ")")
 			return
@@ -169,7 +167,7 @@ func specificJson(p PluginInput) (pi PluginInput) {
 
 		mergo.Merge(&pi, p)
 	} else {
-		pi  = p
+		pi = p
 	}
 
 	pi.generateUserID()
@@ -193,10 +191,8 @@ func doPluginTest(pluginName string) (output Output) {
 		return
 	}
 
-
 	var pluginOutput interface{}
 	json.Unmarshal(out, &pluginOutput)
-
 
 	output.O = json.RawMessage(out)
 	if !*machineReadable {
@@ -246,7 +242,6 @@ func printOutput(o Output) {
 	}
 	return
 }
-
 
 func main() {
 	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
