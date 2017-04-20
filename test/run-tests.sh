22250fa1e4ff39d674debb8fbc7bac29b1136100
Lukas Burgey <ubedv@student.kit.edu>
2017-04-20 14:09:19 +0200

    Rework the run-test script so it uses the new tests command

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -11,87 +11,44 @@
 # Parameters (passed as environment variables)
 : ${TARGET_PLUGIN_REPO:?"Environment variable unset"}
 
+CONFIG=test/config.json
 
 setup_plugin() {
-	echo '==>' "Obtaining WaTTS plugin from $TARGET_PLUGIN_REPO"
+	echo "==> Obtaining WaTTS plugin from $TARGET_PLUGIN_REPO" >&2
 	if [[ -d plugin/.git ]]
-	then git -C plugin pull $TARGET_PLUGIN_REPO || exit
-	else git clone $TARGET_PLUGIN_REPO plugin || exit
-	fi
-	pushd plugin > /dev/null
-	trap 'popd >/dev/null' RETURN
-
-	CONFIG=$(cat test/config.json | jq -cM .)
-
-	CONFIG_INIT_CMD=$(echo $CONFIG | jq -r '.init_cmd')
-	CONFIG_EXEC=$(echo $CONFIG | jq -r '.exec_file')
-	CONFIG_TEST_DIR=$(echo $CONFIG | jq -r '.test_dir')
-
-	[[ $CONFIG_INIT_CMD == null ]] && CONFIG_INIT_CMD=
-	[[ $CONFIG_TEST_DIR == null ]] && CONFIG_TEST_DIR='test'
-	if [[ $CONFIG_EXEC == null ]]
-	then echo '==> Invalid configuration in test/config_json: exec_file unset' >&2 && exit 1
+	then git -C plugin pull $TARGET_PLUGIN_REPO &>/dev/null || exit
+	else git clone $TARGET_PLUGIN_REPO plugin &>/dev/null || exit
 	fi
 }
 
 setup_plugin_tester() {
-	echo '==>' "Building WaTTS plugin tester"
-	./utils/compile.sh || exit
-}
+	echo '==> Building WaTTS plugin tester' >&2
+	PACKAGE=github.com/indigo-dc/watts-plugin-tester
 
-test_plugin() {
-	plugin=$1
-	test=$2
-	action=`echo $test | jq -r .action`
-	input=`echo $test | jq -r .input`
-	expected=`echo $test | jq -r .expected_result`
-	name=`echo $test | jq -r .name`
-
-	list=$(cat test_results.json)
-	output=$(./watts-plugin-tester test $plugin --plugin-action=$action --json=$input_file -m)
-	if [[ $(echo $output | jq -r '.result') == $expected ]]
-	then echo -n '.'
-	else echo -n 'F'
+	if ! which go >/dev/null
+	then
+		echo "go missing, please install go 1.5 or newer" >&2	
+		exit 1
 	fi
 
-	jq --null-input --compact-output \
-		--argjson list "$list" \
-		--argjson output "$output" \
-		--arg expected "$expected" \
-		--arg name "$name" \
-       '$list+[{output: $output, expected_result:$expected, test_name:$name}]' \
-		> test_results.json
+	if [[ -z $GOPATH ]]
+	then
+		export GOPATH=`pwd -P`/gopath
+		echo using local GOPATH $GOPATH >&2
+	fi
+
+	go get $PACKAGE || exit
+	go build -o watts-plugin-tester $PACKAGE || exit
 }
 
-run_tests_from_config() {
-	trap 'rm -f found_at_least_one_input' EXIT
-	echo '[]' > test_results.json
-
-	echo '==> Starting tests'
-	echo $CONFIG | jq -c '.tests[]' | while read test
-	do
-		test_plugin plugin/$CONFIG_EXEC $test
-	done
-	echo
+run_tests() {
+	echo '==> Starting tests' >&2
+	cd plugin
+	watts-plugin-tester -m tests $CONFIG
 }
 
-report_results() {
-	echo -n '==> Finished testing (fail/pass/total): '
-	jq '[(map(select(.output.result!=.expected_result)) | length),
-	(map(select(.output.result==.expected_result)) | length),
-	length] | map(tostring) | join("/")' \
-		-r test_results.json
-
-	echo '==> Results of failed tests (if any)'
-	jq 'map(select(.output.result!=.expected_result)) | .[]' \
-		-r test_results.json \
-		| jq -j '"==> "+.test_name+" returned "+.output.result+", but expected "+.expected_result+" ==> ",.'
-	echo
-}
 
 
 setup_plugin_tester || exit
 setup_plugin || exit
-run_tests_from_config || exit
-report_results || exit
-[[ $(jq 'map(select(.output.result!=.expected_result)) | length == 0' -r test_results.json) == true ]]
+run_tests || exit
